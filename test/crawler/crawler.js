7621a17c5448c294a73688106431708c5b300d46
andy-ax <andy9887776666@gmail.com>
2017-01-09 18:33:57 +0800

    test

--- test/crawler/crawler.js
+++ test/crawler/crawler.js
@@ -1,14 +1,123 @@
-// var cheerio = require('cheerio');
+var cheerio = require('cheerio');
 var https = require("https");
 var file = require("../../bin/helper/file.js");
 
 var url = 'https://www.zhihu.com/explore';
 
 https.get(url, function (res) {
-    var html = '';
+    var html = [];
     res.on('data', function (data) {
-        html += data;
+        html.push(data);
     }).on('end', function () {
-        file.saveFile(html,'./test/crawler/file/zhihuExplore.html','utf-8');
+        loadEnd();
+
+        var buf = new Buffer(Buffer.concat(html), 'utf-8');
+        var str = buf.toString('utf-8');
+
+        var config = {
+            anchor: '.anchor',
+            tabPanel: {
+                index: '.tab-panel',
+                child: {
+                    feedItem: {
+                        index: '.feed-item',
+                        child: {
+                            jsVoteCount: '.js-vote-count',
+                            jsToggleCommentBox: '.js-toggleCommentBox',
+                            questionLink: '.question_link',
+                            authorLinkLine: '.author-link-line',
+                            content: '.content'
+                        }
+                    }
+                }
+            }
+        };
+        var text = getText(config,str);
+        analyzeEnd();
+
+        saveFile2Json(text,'./test/crawler/file/zhiHuHot','utf-8',function () {
+            saveEnd();
+        },function () {
+            saveErr();
+        });
+    }).on('error', function () {
+        loadErr();
     })
 });
+
+/*
+ * {
+ *  title: id
+ *  context: {
+ *      index: id||class
+ *      child: [
+ *          c1: id,
+ *          c2: class,
+ *          c3: {
+ *              index: class
+ *              child:[]
+ *          }
+ *      ]
+ *  }
+ * }
+ */
+function getText(config, html, root) {
+    var $ = cheerio.load(html);
+
+    var text = {};
+    root = root || 'body';
+    deepLoad(config,text,$(root));
+    return text;
+}
+
+function deepLoad(obj, fatherObj, father) {
+    for (var i in obj) {
+        if (obj[i] instanceof Object) {
+            var thisFather = father.find(obj[i].index);
+            fatherObj[i] = [];
+            for (var j = 0,len = thisFather.length; j < len; j++) {
+                var arr = {};
+                deepLoad(obj[i].child, arr, thisFather.eq(j));
+                fatherObj[i].push(arr);
+            }
+        } else {
+            var textElement = father.find(obj[i]);
+            if (textElement.length > 1) {
+                fatherObj[i] = [];
+                for (var j = 0,len = textElement.length; j < len; j++) {
+                    fatherObj[i].push(textElement.eq(j).text());
+                }
+            } else {
+                fatherObj[i] = textElement.text();
+            }
+        }
+    }
+}
+
+function obj2Html(obj, config) {
+
+}
+
+function saveFile2Json(path, obj, encode, resolve, reject) {
+    file.saveFile(JSON.stringify(obj), path + '.json', encode, resolve, reject);
+}
+
+function saveFile2Html(path, obj, encode, config, resolve, reject) {
+
+}
+
+function saveEnd() {
+    console.log('文件已写入');
+}
+function saveErr() {
+    console.log('文件写入出错');
+}
+function loadEnd() {
+    console.log('网页读取完毕')
+}
+function loadErr() {
+    console.log('网页读取出错')
+}
+function analyzeEnd() {
+    console.log('网页分析完毕')
+}
--- test/mongodb/mongos.js
+++ test/mongodb/mongos.js
@@ -4,9 +4,46 @@ var db = new mongodb.Db('mydb',server,{safe:true});
 db.open(function(err,db){
     if(!err)
     {
-        console.log('connect');
+        db.createCollection('mycoll', {}, function (err, collection) {
+            if (err) {
+                console.error(err);
+            } else {
+                //新增数据
+                var tmp1 = {id:1,title:'hello',number:1};
+                collection.insert(tmp1,{}, function (err, result) {
+                    console.log('end');
+                });
+                //更新数据
+                collection.update({title:'hello'},{$set: {number: 3}},{},function (err, result) {
+                    console.log("update success");
+                });
+                //查询数据
+                collection.find().toArray(function (err, docs) {
+                    console.log('find all');
+                    console.log(docs);
+                });
+                collection.findOne(function (err, doc) {
+                    console.log('find one');
+                    console.log(doc);
+                });
+                //删除数据
+                collection.remove({title:'hello'},{},function (err, result) {
+                    console.log('remove');
+                    console.log(result);
+                });
+            }
+        });
+        //删除 collection
+        db.dropCollection('mycoll',{},function (err, result) {
+            if (err) {
+                console.log('err');
+                console.log(err);
+            } else {
+                console.log('result');
+                console.log(result);
+            }
+        })
     }else{
-        console.log(err);
+        console.error(err);
     }
-
 });
