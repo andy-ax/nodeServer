bdfb2b9e69d8e6affb1110946a20c57bdeee8cb5
andy-ax <andy9887776666@gmail.com>
2017-01-11 22:58:53 +0800

    test

--- test/mongodb/mongos.js
+++ test/mongodb/mongos.js
@@ -60,8 +60,8 @@ var init2 = function () {
     //定义模式
     var UserSchema = new mongoose.Schema({
         username: String,
-        name: String,
-        password: String
+        friend: String,
+        msg: String
     });
 
     //定义模型
@@ -81,19 +81,99 @@ var init2 = function () {
         })
     }
     /*
+     * mongodb初始化数据
+     * model = new Model({data});
+     * 插入
+     * model.save(callback);
+     *
+     * 另一种初始化与保存方式
+     * Model.create({data},callback(err,small))
+     */
+
+    function save1() {
+        var user = new User({
+            username: 'andy',
+            friend: 'andy mars',
+            msg: '1234567'
+        });
+        user.save(function (err, data, raw) {
+
+        });
+    }
+    function save2(data) {
+        User.create(data,function (err, data) {
+
+        });
+    }
+    // save2({
+    //     username: 'andy',
+    //     friend: 'mars',
+    //     msg: '1'
+    // });
+    // save2({
+    //     username: 'andy',
+    //     friend: 'paul',
+    //     msg: '2'
+    // });
+    // save2({
+    //     username: 'andy',
+    //     friend: 'joan',
+    //     msg: '3'
+    // });
+    // save2({
+    //     username: 'andy',
+    //     friend: 'david',
+    //     msg: '4'
+    // });
+
+    /*
      * mongodb数据查找
-     * model.find([查询索引], [回调])
-     * model.findOne([查询索引], [回调])
+     * Model.find([查询索引], [回调])
+     * Model.findOne([查询索引], [回调])
      */
     /*
-     * 查询排序result.sort([排序索引],[1|-1]).exec([callback]);
+     * 查询排序result.sort([排序索引]).exec([callback]);
      * 1->正序 -1->倒序
      */
     /*
      * 查询部分结果
-     * .skip(查询的数据数量) 查询一部分数据
-     * .limit(一页数据的数量) 数据分页
+     * .limit(number) 查询一部分数据
      */
+
+    function find() {
+        User.find({username:'andy'}).sort('msg').limit(2).exec(function (err, datas) {
+            console.log(datas);
+        })
+    }
+
+    /*
+     * 移除数据
+     * Model.remove({data},callback(err));
+     */
+    function remove() {
+        User.remove({username:'andy'}).exec(function (err, user) {
+
+        })
+    }
+
+    /*
+     * 更新数据
+     * Model.update(查询条件,更新对象{$set: {更新的部分}},option,callback(err,numberAffected,raw));
+     * 更新对象是数据的全部，如果只更新部分，可以使用set更新部分\
+     * numberAffected -> {n:number总数, nModified:number修改的数量, ok:number成功的数量}
+     *
+     * 更新并获取要更新的对象
+     * findOneAndUpdate
+     */
+    function update() {
+        User.update({friend:'mars'},{$set: {msg: 'hello!'}},function (err, numberAffected) {
+            console.log(numberAffected);
+        });
+        User.find({friend:'mars'}).exec(function (err, datas) {
+            console.log(arguments);
+        })
+    }
+    update();
 };
 
 init2();
